15a7b8a57930b93c2e45b89de414a8dfe1ab6070
Gillian Brousse <gbrousse@office.unimes.fr>
2024-03-19 16:13:01 +0100

    trying new fetch method for articles and FAQ questions 3

--- src/01-App.jsx
+++ src/01-App.jsx
@@ -45,6 +45,8 @@ let Data07 =
 export default function App() {
   //Get datas
   const [Loading, setLoading] = useState(true);
+  const [Loading2, setLoading2] = useState(true);
+  const [dom, setDom] = useState(false);
   const [Data1, setData1] = useState({});
   const [Data2, setData2] = useState({});
   const [Data3, setData3] = useState({});
@@ -55,6 +57,7 @@ export default function App() {
 
   let allFaq = [];
   let allArticle = [];
+  const [array, setArray] = useState([]);
 
   //axios config
   const request1 = axios.get(Data01);
@@ -63,6 +66,7 @@ export default function App() {
   const request4 = axios.get(Data06);
   const request5 = axios.get(Data07);
   //Get and store datas before rendering
+
   useEffect(() => {
     async function getDatas() {
       axios.all([request1, request2, request3, request4, request5]).then(
@@ -79,14 +83,43 @@ export default function App() {
           setData5(responseThree.data);
           setData6(responseFour.data);
           setData7(responseFive.data);
+          console.log("done");
           setLoading(false);
         }),
       );
     }
+
     getDatas();
   }, []);
 
+  useEffect(() => {
+    async function getDatas2() {
+      console.log("coucou");
+      let allDownloadurls1 = [];
 
+      for (let i in Data7) {
+        let thisData = Data7[i].download_url;
+        allDownloadurls1.push(thisData);
+      }
+      let allArticleDatas = [];
+
+      for (let i in allDownloadurls1) {
+        let thisData = allDownloadurls1[i];
+        const DataFetch = await axios.get(thisData);
+        setArray((oldArray) => [...oldArray, DataFetch.data]);
+      }
+    }
+
+    getDatas2();
+  }, [Loading]);
+
+  console.log(array);
+  console.log(array.length);
+  if (array.length >= 70) {
+    console.log("ok");
+    setLoading2(false);
+    setDom(true);
+  }
   /*
   console.log(Data1);
   console.log(Data2);
@@ -94,71 +127,72 @@ export default function App() {
   console.log(Data4);
   console.log(Data5);
   console.log(Data6);
-  */
-
-  //console.log(Data7);
-
-
+    console.log(Data7);
+    */
 
   //Check if loading is complete before rendering
-  if (Loading) {
+  if (Loading2) {
     return <Loader />;
   }
   //If loading is complete, render DOM
   else {
-      getDatas2();
-  getDatas3();
+    if (dom) {
+      /*
+  getDatas2();
+getDatas3();
 
-  async function getDatas2() {
-    for (let i in Data6) {
-      let thisData = Data6[i].download_url;
-      const DataFetch = await axios.get(thisData);
-      const DataFetch2 = DataFetch.data;
-      allFaq.push(DataFetch2);
-    }
+async function getDatas2() {
+  for (let i in Data6) {
+    let thisData = Data6[i].download_url;
+    const DataFetch = await axios.get(thisData);
+    const DataFetch2 = DataFetch.data;
+    allFaq.push(DataFetch2);
   }
+}
 
-  async function getDatas3() {
-    for (let i in Data7) {
-      let thisData = Data7[i].download_url;
-      //console.log(thisData)
-      const DataFetch = await axios.get(thisData);
-      const DataFetch2 = DataFetch.data;
-      allArticle.push(DataFetch2);
-    }
+async function getDatas3() {
+  for (let i in Data7) {
+    let thisData = Data7[i].download_url;
+    //console.log(thisData)
+    const DataFetch = await axios.get(thisData);
+    const DataFetch2 = DataFetch.data;
+    allArticle.push(DataFetch2);
   }
+}
 
-    //console.log(allFaq);
-  console.log(allArticle);
-  console.log(allArticle[5]);
-    //DOM
-    return (
-      <Router>
-        <div className="app">
-          {/*ROUTES*/}
-          <Switch>
-            {/*DATA*/}
-            <Route exact path="/etat-service.php"></Route>
-            {/*ACCUEIL*/}
-            <Route exact path="/accueil">
-              <Homepage />
-            </Route>
-            {/*CATALOGUE*/}
-            <Route
-              exact
-              path={[
-                "/catalogue-etudiants",
-                "/catalogue-enseignants-chercheurs",
-                "/catalogue-personnels",
-              ]}
-            >
-              <Header data1={CheckProfile()} />
-              <Catalogue data1={Data1} data2={allArticle} />
-              <Footer data={Data4} />
-            </Route>
+//console.log(allFaq);
+console.log(allArticle);
+console.log(allArticle[5]);
 
-            {/*FAQ*/}
-            {/*           <Route
+  */
+      //DOM
+      return (
+        <Router>
+          <div className="app">
+            {/*ROUTES*/}
+            <Switch>
+              {/*DATA*/}
+              <Route exact path="/etat-service.php"></Route>
+              {/*ACCUEIL*/}
+              <Route exact path="/accueil">
+                <Homepage />
+              </Route>
+              {/*CATALOGUE*/}
+              <Route
+                exact
+                path={[
+                  "/catalogue-etudiants",
+                  "/catalogue-enseignants-chercheurs",
+                  "/catalogue-personnels",
+                ]}
+              >
+                <Header data1={CheckProfile()} />
+                <Catalogue data1={Data1} data2={allArticle} />
+                <Footer data={Data4} />
+              </Route>
+
+              {/*FAQ*/}
+              {/*           <Route
               exact
               path={[
                 "/foire-aux-questions-etudiants",
@@ -172,63 +206,64 @@ export default function App() {
             </Route>
 */}
 
-            {/*ASSISTANCE*/}
-            <Route
-              exact
-              path={[
-                "/assistance-etudiants",
-                "/assistance-enseignants-chercheurs",
-                "/assistance-personnels",
-              ]}
-            >
-              <Header data1={CheckProfile()} />
-              <Assistance data1={CheckProfile()} data2={Data5} />
-              <Footer data={Data4} />
-            </Route>
+              {/*ASSISTANCE*/}
+              <Route
+                exact
+                path={[
+                  "/assistance-etudiants",
+                  "/assistance-enseignants-chercheurs",
+                  "/assistance-personnels",
+                ]}
+              >
+                <Header data1={CheckProfile()} />
+                <Assistance data1={CheckProfile()} data2={Data5} />
+                <Footer data={Data4} />
+              </Route>
 
-            {/*SEARCH*/}
-            <Route
-              exact
-              path={[
-                "/recherche-etudiants",
-                "/recherche-enseignants",
-                "/recherche-personnels",
-              ]}
-            >
-              <Header data1={CheckProfile()} />
-              <Search data1={Data3} />
-              <Footer data={Data4} />
-            </Route>
-            {/*ARTICLES*/}
-            {GetArticlesLinks(Data03).map((item) => (
-              <Route key={"1-" + item} exact path={"/etudiants/" + item}>
+              {/*SEARCH*/}
+              <Route
+                exact
+                path={[
+                  "/recherche-etudiants",
+                  "/recherche-enseignants",
+                  "/recherche-personnels",
+                ]}
+              >
                 <Header data1={CheckProfile()} />
-                <Article data1={Data3} data2={Data2} />
+                <Search data1={Data3} />
                 <Footer data={Data4} />
               </Route>
-            ))}
-            {GetArticlesLinks(Data03).map((item) => (
-              <Route key={"2-" + item} exact path={"/enseignants/" + item}>
-                <Header data1={CheckProfile()} />
-                <Article data1={Data3} data2={Data2} />
-                <Footer data={Data4} />
+              {/*ARTICLES*/}
+              {GetArticlesLinks(Data03).map((item) => (
+                <Route key={"1-" + item} exact path={"/etudiants/" + item}>
+                  <Header data1={CheckProfile()} />
+                  <Article data1={Data3} data2={Data2} />
+                  <Footer data={Data4} />
+                </Route>
+              ))}
+              {GetArticlesLinks(Data03).map((item) => (
+                <Route key={"2-" + item} exact path={"/enseignants/" + item}>
+                  <Header data1={CheckProfile()} />
+                  <Article data1={Data3} data2={Data2} />
+                  <Footer data={Data4} />
+                </Route>
+              ))}
+              {GetArticlesLinks(Data03).map((item) => (
+                <Route key={"3-" + item} exact path={"/personnels/" + item}>
+                  <Header data1={CheckProfile()} />
+                  <Article data1={Data3} data2={Data2} />
+                  <Footer data={Data4} />
+                </Route>
+              ))}
+              {/*REDIRECT*/}
+              <Route>
+                <Redirect to="/accueil" />
               </Route>
-            ))}
-            {GetArticlesLinks(Data03).map((item) => (
-              <Route key={"3-" + item} exact path={"/personnels/" + item}>
-                <Header data1={CheckProfile()} />
-                <Article data1={Data3} data2={Data2} />
-                <Footer data={Data4} />
-              </Route>
-            ))}
-            {/*REDIRECT*/}
-            <Route>
-              <Redirect to="/accueil" />
-            </Route>
-          </Switch>
-          <p className="downCop">Université de Nîmes - DSIUN</p>
-        </div>
-      </Router>
-    );
+            </Switch>
+            <p className="downCop">Université de Nîmes - DSIUN</p>
+          </div>
+        </Router>
+      );
+    }
   }
 }
